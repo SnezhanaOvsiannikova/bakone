d2525ae1c5c9cdc47c511432ae223d8b421957bf
jack ovsiannikov <jack ovsiannikov>
2017-01-06 16:24:33 +0200

    fix a page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,41 @@
 (function() {
-    $('.imgScale').imageScale({ 
-        align: 'center'
-    });
+	$('.imgScale').imageScale({ 
+		align: 'center'
+	});
 }());
+
+$(document).ready(function(){
+	$(".scroll-to").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+
+var version = detectIE();
+
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+  return false;
+}
+$('body').addClass(version);
+if (version >= 9) {
+	$('body').addClass('ie');
+}
